ba68ce212d4f4fe3b51e13cb188120a8ecd1ae3a
Yeonju Go <yeonjugo@gmail.com>
2014-12-16 15:41:35 +0900

    JEC residual correction, first commit

---
+++ 141215_residualJEC/run_draw_JEC.sh
@@ -0,0 +1,15 @@
+#root -l -b -q 'draw_JEC.C+("ak3PF",1)'
+#root -l -b -q 'draw_JEC.C+("ak4PF",1)'
+#root -l -b -q 'draw_JEC.C+("ak5PF",1)'
+
+root -l -b -q 'draw_JEC.C+("akPu3PF",1)'
+root -l -b -q 'draw_JEC.C+("akPu4PF",1)'
+root -l -b -q 'draw_JEC.C+("akPu5PF",1)'
+
+#root -l -b -q 'draw_JEC.C+("ak3Calo",1)'
+#root -l -b -q 'draw_JEC.C+("ak4Calo",1)'
+#root -l -b -q 'draw_JEC.C+("ak5Calo",1)'
+
+root -l -b -q 'draw_JEC.C+("akPu3Calo",1)'
+root -l -b -q 'draw_JEC.C+("akPu4Calo",1)'
+root -l -b -q 'draw_JEC.C+("akPu5Calo",1)'
